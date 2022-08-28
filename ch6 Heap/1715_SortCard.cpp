9a9b445a982a9bbeabd77a0337b426dbbd4190a4
Siru <ehdgur9553@naver.com>
2022-08-29 08:17:22 +0900

    Update 1715_SortCard.cpp

--- ch6 Heap/1715_SortCard.cpp
+++ ch6 Heap/1715_SortCard.cpp
@@ -26,11 +26,14 @@ using namespace std;
 
 int main(void)
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
     int n;
     long long int ans = 0;
 
     priority_queue<long long int> deck;
-    
+
     cin >> n;
     for(int i = 0; i < n; i++)
     {

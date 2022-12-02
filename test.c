8f67a246786c91e059e227c3985bf1a1fa4b64bc
Siru <ehdgur9553@naver.com>
2022-12-02 09:36:35 +0900

    test commit and push

--- test.c
+++ test.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+int tempA = 5;
+char tempB[10] = {"hello"};
+
+void SubNum(int a, int b)
+{
+    int c;
+    c = a - b;
+    printf("%d\n", c);
+}
+
 int main()
 {
-    
+    int tempC = 3;
 
-    
+    int solution = tempA + tempC;
+    printf("%d\n", solution);
+    printf("%s\n", tempB);
+    SubNum(tempA, tempC);
+
+    return 0;   
 }
